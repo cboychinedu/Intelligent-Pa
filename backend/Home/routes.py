be43270677e03495844634b1b4728a2cf1ac5ddc
Mbonu Chinedum <cboy.chinedu@gmail.com>
2024-08-17 23:58:26 +0100

    --route.py

--- backend/Home/routes.py
+++ backend/Home/routes.py
@@ -2,11 +2,16 @@
 
 # Importing the necessary modules 
 import os 
+import bcrypt
+from mongo import MongoDB
 from flask import request, jsonify
 from flask import Blueprint, session 
 
 # Creating the blueprint object 
 home = Blueprint('home', __name__, template_folder='templates', static_folder='static');
+
+# Creating an instance of the database 
+db = MongoDB(); 
 
 # Creating the home page 
 @home.route('/', methods=["POST"])
@@ -17,7 +22,11 @@ def Register():
     email = requestData["emailAddress"]
     password = requestData["password"]
 
-    #  
+    # Connecting to the Mongodb database, and save the users data 
+    db.connect('mongodb://localhost:27017/', 'intelligent_pa')
+
+    # 
+    databaseData = db.retrieve_data('users', email=email)
 
 
     # Register the user 
